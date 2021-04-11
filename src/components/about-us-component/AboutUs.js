e01395cc475f87f5f8bc2ebd4c9666dde7df2aae
Zana Garaplija <zana.garaplija@edu.fit.ba>
2021-04-11 22:43:20 +0200

    style changes

--- src/components/about-us-component/AboutUs.js
+++ src/components/about-us-component/AboutUs.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import styles from './StyleAboutUs.js';
 import TextComponent from "../../components/text-component/TextComponent";
 
 const AboutUs = () => {
     return (
-        <div style={styles.outerWrapper}>
-            <TextComponent style={styles.text}>
+        <div className="aboutUs">
+            <TextComponent>
                 Lucky market je domaci sajam veleoproizvodnje 
                 Lucky market je domaci sajam veleoproizvodnje jshadfh hasdf hlsadhfb ljhasdlfhhd  hasdfh. sdahfhasljdh lasdhbf. hsgfhgasldfh jfsdh
                 Lucky market je domaci sajam veleoproizvodnje jshadfh hasdf hlsadhfb ljhasdlfhhd  hasdfh. sdahfhasljdh lasdhbf. hsgfhgasldfh jfsdh
